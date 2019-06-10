19bc9f6972dd7318518db384caf1eb1af4642b76
tjlinares <tjlinares23@gmail.com>
2019-06-10 10:22:26 -0400

    Updates

---
+++ precache-manifest.9df397bfc1a5cd0c954a07f64d0b59ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb63f67abbcda642df9dae29dd11425",
+    "url": "/zipcode-projec/index.html"
+  },
+  {
+    "revision": "e95d156875757015f44d",
+    "url": "/zipcode-projec/static/css/main.9409709c.chunk.css"
+  },
+  {
+    "revision": "9db6b1bc215d94ebf7d6",
+    "url": "/zipcode-projec/static/js/2.9b42cba6.chunk.js"
+  },
+  {
+    "revision": "e95d156875757015f44d",
+    "url": "/zipcode-projec/static/js/main.7e9dd698.chunk.js"
+  },
+  {
+    "revision": "a590da7695b0e00c6d07",
+    "url": "/zipcode-projec/static/js/runtime~main.fecfd84a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zipcode-projec/precache-manifest.9df397bfc1a5cd0c954a07f64d0b59ac.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zipcode-projec/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
